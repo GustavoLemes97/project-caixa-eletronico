84e0c8dabe0d15eaae4bf5d808ef8fdc8efa338e
Gustavo Lemes <gustavolemes997@gmail.com>
2022-09-13 19:53:54 -0300

    Cria métodos da classe Transacao

--- src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
@@ -1,6 +1,36 @@
 package com.trybe.acc.java.caixaeletronico;
 
-public class Transacao {
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Transacao {
+  private double quantia;
+  private String instante;
+  private String descricao;
+  private Conta conta;
+
+  public static final DateTimeFormatter PADRAO_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+  public Transacao(double quantia, String descricao) {
+    this.quantia = quantia;
+    this.descricao = descricao;
+    this.instante = retornarInstante();
+  }
+
+  public String retornarInstante() {
+    LocalDateTime dataAtual = LocalDateTime.now();
+    return PADRAO_DATA.format(dataAtual);
+  }
+
+  public double getQuantia() {
+    return this.quantia;
+  }
+
+  public String retornarResumoTransacao() {
+    return "Horário da Transação: " + this.instante
+            + " Quantia: " + this.quantia
+            + " Descrição: " + this.descricao;
+  }
 }
 
