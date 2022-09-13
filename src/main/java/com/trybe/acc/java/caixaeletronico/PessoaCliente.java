e6d00fbf25643c6c606e44a0a0ffbca8ecabe892
Gustavo Lemes <gustavolemes997@gmail.com>
2022-09-13 19:29:58 -0300

    Cria métodos da classe PessoaCliente

--- src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
@@ -13,10 +13,39 @@ public class PessoaCliente {
     this.nomeCompleto = nomeCompleto;
     this.cpf = cpf;
     this.senha = senha;
+
+    System.out.println("Nova pessoa cliente " + nomeCompleto + " com CPF: " + cpf + " criada!");
   }
 
+  public void adicionarConta(Conta novaConta) {
+    this.contas.add(novaConta);
+  }
+
+  public int retornaNumeroDeContas() {
+    return this.contas.size();
+  }
+
+  public double retornarSaldoContaEspecifica(int conta) {
+    return this.contas.get(conta).retornarSaldo();
+  }
+
+  public String retornarIdContaEspecifica(int conta) {
+    return this.contas.get(conta).getIdConta();
+  }
+
+  public void retornarExtratoContaEspecifica(int conta) {
+    this.contas.get(conta).retornarExtrato();
+  }
+
+  public void adicionarTransacaoContaEspecifica(int conta, double quantia, String descricao) {
+    this.contas.get(conta).adicionarTransacao(quantia, descricao);
+  }
+
+  public void retornarResumoContas() {
+    this.contas.stream().map(Conta::retornarResumoConta);
+  }
   public String getCpf() {
-    return cpf;
+    return this.cpf;
   }
 
   public boolean validarSenha(String senha) {
